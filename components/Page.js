a8f6dd5155201d535de180fd5601744ed64c7d3f
Hafeez <feeziman007@hotmail.com>
2018-12-18 09:47:03 +1100

    moved all files to the root

--- components/Page.js
+++ components/Page.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 
 import Header from "./Header";
 import Meta from "./Meta";
@@ -24,6 +24,35 @@ const Inner = styled.div`
   margin: 0 auto;
   padding: 2rem;
 `;
+
+createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+  button {  font-family: 'radnika_next'; }
+`;
+
 class Page extends Component {
   render() {
     return (
